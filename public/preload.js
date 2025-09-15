61279e840bc3cbba381922f250035f277f08df8b
jaspreet <jaspreet@jaspreets-MacBook-Air.local>
2025-09-15 16:57:17 +0530

    Updates

--- public/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  loadExcelFile: () => ipcRenderer.invoke('load-excel-file'),
-  selectExcelFile: () => ipcRenderer.invoke('select-excel-file'),
-  onAutoLoadExcel: (callback) => ipcRenderer.on('auto-load-excel-file', callback),
-  removeAutoLoadExcel: (callback) => ipcRenderer.removeListener('auto-load-excel-file', callback)
-});
